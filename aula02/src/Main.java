c5f547aa50b45a203c6fb9006691ecbee1f9febe
Quezia Miranda Pinheiro dos anjos <142028824+queziamirandaDev@users.noreply.github.com>
2025-07-15 17:23:06 -0300

    Add files via upload

---
+++ aula02/src/Main.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        // Solicita o nome do usuário
+        System.out.print("Digite seu nome: ");
+        String nome = scanner.nextLine();
+
+        System.out.print("Welcome! " + nome);
+
+
+
+
+       }
+
+    }
---
+++ aula02/src/receita.java
@@ -0,0 +1,11 @@
+public class receita {
+    // atalho psvm
+    public static void main(String[] args) {
+
+        // atalho sout
+        // /n pular linha
+
+        System.out.println();
+
+    }
+}
